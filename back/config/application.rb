a718cdb53c411a7c74cc58e9b8d2ffe9d1b87747
RTa-technology <scpfoundationidcardmaker@gmail.com>
2023-05-09 17:46:49 +0900

    :tada: front

--- back/config/application.rb
+++ back/config/application.rb
@@ -30,11 +30,9 @@ module Back
     config.middleware.use ActionDispatch::Flash
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        # 今回はRailsのポートが3001番、Reactのポートが3000番にするので、Reactのリクエストを許可するためにlocalhost:3000を設定
-        origins 'localhost:3000'
+        origins 'localhost:8001'
         resource '*',
                  :headers => :any,
-                 # この一文で、渡される、'access-token'、'uid'、'client'というheaders情報を用いてログイン状態を維持する。
                  :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
                  :methods => [:get, :post, :options, :delete, :put]
       end
